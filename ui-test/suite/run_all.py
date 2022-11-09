9ce37a40173d914689277b4a28852e5fd2db0221
ouyangwenjing <1036611218@qq.com>
2022-11-09 18:51:28 +0800

    修改路径

--- ui-test/suite/run_all.py
+++ ui-test/suite/run_all.py
@@ -8,7 +8,7 @@ from util.report_tool import ReportTool
 # report_path = os.path.abspath(os.path.dirname(__file__))[
 #               :os.path.abspath(os.path.dirname(__file__)).find("test-framework") + len(
 #                   "test-framework")] + "/ui-test" + config['html']['htmlfile_path']
-report_path = os.path.abspath(os.path.dirname(__file__))+ config['html']['htmlfile_path']
+report_path = os.path.abspath(os.path.dirname(__file__)) + config['html']['htmlfile_path']
 print(report_path)
 # 报告名字
 report_name = config['html']["htmlfile_name"]
